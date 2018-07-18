8fa9b26449a95c4b5d27e19b85a4f24079abc81e
manojnaidu15498 <40794003+manojnaidu15498@users.noreply.github.com>
2018-07-18 15:55:37 +0530

    Create p9.py

---
+++ p9.py
@@ -0,0 +1,16 @@
+a=int(input())
+b=input().split()
+c=[]
+f=0
+for i in range(a):
+    c.append(int(b[i]))
+for i in range(a):
+    for j in range(a):
+        if(i!=j):
+            d=c[i]+c[j]
+            if(d==0):
+                print(str(c[i])+" "+str(c[j]))
+                f=f+1
+        break
+if(f==0):
+    print("-1 2")
